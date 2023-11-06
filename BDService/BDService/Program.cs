7b940da97289b44160d37a1a31964fce39478e77
Marina <SemenovaMN@bnipi.rosneft.ru>
2023-11-06 19:20:30 +0500

    bdservice

---
+++ BDService/BDService/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Text;
+using Newtonsoft.Json;
+using Npgsql;
+using NpgsqlTypes;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BDService
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine(Environment.GetEnvironmentVariable("RbName"));
+            //Console.WriteLine(Environment.GetEnvironmentVariable("RbServer"));
+            //Console.WriteLine(Environment.GetEnvironmentVariable("RbLogin"));
+            //Console.WriteLine(Environment.GetEnvironmentVariable("RbPassword"));
+
+            yeqon.Settings settings = new yeqon.Settings
+            {
+                Name = "doc_db",
+                Queue = "doc_db",
+                Server = "127.0.0.1",
+                Login = "debug",
+                Password = "debug",
+                NoAck = false,
+                Durable = true
+            };
+
+            var listener = new yeqon.Listener();
+            listener.Init(settings);
+            listener.OnMessage += (data, id, reply_to) =>
+            {
+                var worker = new Task(async () =>
+                {
+                    var resp = "";
+                    listener.Reply(id, reply_to, Encoding.UTF8.GetBytes(resp));
+                });
+                worker.Start();
+            };
+            await listener.Run();
+        }
+
+        
+    }
+}
