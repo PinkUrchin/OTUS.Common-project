2742645d775404fd164e48d6bd2a9fcee3f95583
Marina <SemenovaMN@bnipi.rosneft.ru>
2023-12-29 15:24:51 +0500

    ...

---
+++ ApiDB/proto/Responses/Shape.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocol.Common
+{
+    public class Shape
+    {
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+        [JsonProperty("ShapeType")]
+        public byte ShapeType { get; set; }
+        [JsonProperty("CreateDate")]
+        public DateTime CreateDate { get; set; }
+        [JsonProperty("UpdateDate")]
+        public DateTime UpdateDate { get; set; }
+        [JsonProperty("CreateAuthor")]
+        public string CreateAuthor { get; set; }
+        [JsonProperty("Color")]
+        public string  Color { get; set; }
+     
+        [JsonProperty("DocumentId")]
+        public int DocumentId { get; set; }
+        [JsonProperty("Coords")]
+        public string Coords { get; set; }
+    }
+}
